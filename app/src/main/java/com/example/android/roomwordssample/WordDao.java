c392fc548ac4630dfbb76967a3c2889ea9634fa4
Yuvaraja <yuvarajaj-wa14@student.tarc.edu.my>
2019-04-16 10:49:12 +0800

    RoomWordsSample

---
+++ app/src/main/java/com/example/android/roomwordssample/WordDao.java
@@ -0,0 +1,27 @@
+package com.example.android.roomwordssample;
+
+
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+
+
+@Dao
+public interface WordDao {
+
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAlphabetizedWords();
+
+    @Insert (onConflict = OnConflictStrategy.IGNORE)
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+}
---
+++ app/src/main/java/com/example/android/roomwordssample/WordRoomDatabase.java
@@ -0,0 +1,71 @@
+package com.example.android.roomwordssample;
+
+
+
+import android.arch.persistence.db.SupportSQLiteDatabase;
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+
+
+
+@Database(entities = {Word.class}, version = 1,  exportSchema = false)
+public abstract class WordRoomDatabase extends RoomDatabase {
+
+    public abstract WordDao wordDao();
+
+    private static WordRoomDatabase INSTANCE;
+
+    static WordRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (WordRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            WordRoomDatabase.class, "word_database")
+
+                            .fallbackToDestructiveMigration()
+                            .addCallback(sRoomDatabaseCallback)
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback(){
+
+        @Override
+        public void onOpen (@NonNull SupportSQLiteDatabase db){
+            super.onOpen(db);
+
+            new PopulateDbAsync(INSTANCE).execute();
+        }
+    };
+
+
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+
+        private final WordDao mDao;
+        String [] words = {"dolphin", "crocodile", "cobra"};
+
+        PopulateDbAsync(WordRoomDatabase db) {
+            mDao = db.wordDao();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... params) {
+
+            mDao.deleteAll();
+
+            for( int i = 0; i <= words.length - 1; i++) {
+                Word word = new Word(words[i]);
+                mDao.insert(word);
+            }
+            return null;
+        }
+    }
+}
